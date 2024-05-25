4be41668396864c8ebcecb032f3ce63354a426aa
timothée picard <timothee.picard@outlook.fr>
2024-05-25 08:27:01 +0200

    integrate project page

--- app/projets/page.tsx
+++ app/projets/page.tsx
@@ -1,15 +1,23 @@
+"use client"
 import Link from "next/link";
+import {useState} from "react";
 
 export default function ProjectsPage() {
+    const [activeFilter, setActiveFilter] = useState("all")
+
+    const handleFilter = (filter: string) => {
+        setActiveFilter(filter)
+    }
+
     return (
         <div className="mx-10 lg:mx-[10rem]">
-            <h1 className="uppercase font-secondary text-9xl italic font-extrabold mb-6 mt-20 text-shadow-title text-stroke text-customPurple" data-text={"Work"}>Work</h1>
+            <h1 className="relative uppercase font-secondary text-9xl italic font-extrabold mb-6 mt-20 text-shadow-title text-stroke text-customPurple" data-text={"Work"}>Work</h1>
             <ul className="flex flex-wrap flex-col md:flex-row uppercase font-semibold text-custompurple gap-5 text-customPurpleDark">
-                <li className="whitespace-nowrap border-4 border-customPurpleDark rounded-full px-8 py-2 shadow-filters cursor-pointer hover:activeFilter activeFilter">Show all</li>
-                <li className="whitespace-nowrap border-4 border-customPurpleDark rounded-full px-8 py-2 shadow-filters cursor-pointer hover:activeFilter">branding</li>
-                <li className="whitespace-nowrap border-4 border-customPurpleDark rounded-full px-8 py-2 shadow-filters cursor-pointer hover:activeFilter">Strategy</li>
-                <li className="whitespace-nowrap border-4 border-customPurpleDark rounded-full px-8 py-2 shadow-filters cursor-pointer hover:activeFilter">graphism</li>
-                <li className="whitespace-nowrap border-4 border-customPurpleDark rounded-full px-8 py-2 shadow-filters cursor-pointer hover:activeFilter">reset</li>
+                <li className={`whitespace-nowrap border-4 border-customPurpleDark rounded-full px-8 py-2 shadow-filters cursor-pointer hover:activeFilter ${activeFilter === "all" ? "activeFilter" : ""}`} onClick={() => handleFilter("all")}>Show all</li>
+                <li className={`whitespace-nowrap border-4 border-customPurpleDark rounded-full px-8 py-2 shadow-filters cursor-pointer hover:activeFilter ${activeFilter === "branding" ? "activeFilter" : ""}`} onClick={() => handleFilter("branding")}>branding</li>
+                <li className={`whitespace-nowrap border-4 border-customPurpleDark rounded-full px-8 py-2 shadow-filters cursor-pointer hover:activeFilter ${activeFilter === "strategy" ? "activeFilter" : ""}`} onClick={() => handleFilter("strategy")}>Strategy</li>
+                <li className={`whitespace-nowrap border-4 border-customPurpleDark rounded-full px-8 py-2 shadow-filters cursor-pointer hover:activeFilter ${activeFilter === "graphism" ? "activeFilter" : ""}`} onClick={() => handleFilter("graphism")}>graphism</li>
+                <li className={`whitespace-nowrap border-4 border-customPurpleDark rounded-full px-8 py-2 shadow-filters cursor-pointer hover:activeFilter ${activeFilter === "reset" ? "activeFilter" : ""}`} onClick={() => handleFilter("reset")}>reset</li>
             </ul>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 mt-32">
                 {[...Array(9)].map((_, index) => (
