ec11a985c19e82132a83994849dc7cd4eaa6739f
timothée picard <timothee.picard@outlook.fr>
2024-09-01 13:05:14 +0200

    home stickers

--- components/CustomTitle.tsx
+++ components/CustomTitle.tsx
@@ -9,7 +9,7 @@ export default function CustomTitle(Props: CustomButtonProps) {
 	const {children, className, secondary} = Props
 
 	return (
-		<h1 className={`relative uppercase font-secondary italic font-extrabold ${secondary? "shadow-title-secondary text-stroke-secondary text-[#84E3DF]" : "shadow-title text-stroke text-customPurple" } ${className}`}
+		<h1 id="title" className={`relative uppercase font-secondary italic font-extrabold ${secondary? "shadow-title-secondary text-stroke-secondary text-[#84E3DF]" : "shadow-title text-stroke text-customPurple" } ${className}`}
 			data-text={children}>
 			{children}
 		</h1>
